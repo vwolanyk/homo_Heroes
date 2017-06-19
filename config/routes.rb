10d4bcb4897773b7db0a709936d3a3628c39b4fd
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-18 23:44:16 -0400

    Create Log in view, destroy method - works

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ get 'queer_icons/new' => 'queer_icons#new'
 get 'queer_icons/:id' => 'queer_icons#show'
 
 resources :users, only: %i(new create)
+resources :sessions, only: %i(new create destroy)
+
+
 
 end
